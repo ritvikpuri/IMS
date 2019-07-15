878d7deb0ee7f2234c848e470ff6ffe562eae3fd
ritvikpuri <43915436+ritvikpuri@users.noreply.github.com>
2019-07-15 14:10:00 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import {Jumbotron, Container, Nav, Button, NavDropdown} from 'react-bootstrap';
+import Dropdown from 'react-bootstrap/Dropdown';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+    render() {
+        return (
+            <Container>
+                <Jumbotron>
+                    <div class="form-group">
+                        <div class="form-group col-md-3">
+                            <Nav defaultActiveKey="/home" className="flex-column">
+                                <Nav.Link href="/home">Admin Dashboard</Nav.Link>
+                                <NavDropdown title="Requests" id="request-dropdown">
+                                    <NavDropdown.Item eventKey="link">New</NavDropdown.Item>
+                                    <NavDropdown.Item eventKey="link">Pending</NavDropdown.Item>
+                                    <NavDropdown.Item eventKey="link">History</NavDropdown.Item>
+                                </NavDropdown>
+                                <NavDropdown title="Inventory" id="inventory-dropdown">
+                                    <NavDropdown.Item eventKey="link">Add Item</NavDropdown.Item>
+                                    <NavDropdown.Item eventKey="link">Inventory List</NavDropdown.Item>
+                                </NavDropdown>
+                                <Nav.Link eventKey="link-2">User</Nav.Link>
+                            </Nav>
+                        </div>
+                    </div>
+                </Jumbotron>
+            </Container>
+        );
+    }
 }
 
 export default App;

af04196e6027cacbf4aec30f08a038c7cde5743c
ritvikpuri <43915436+ritvikpuri@users.noreply.github.com>
2019-07-18 12:31:18 +0530

    Add files via upload

--- src/components/SignupPage.js
+++ src/components/SignupPage.js
@@ -55,7 +55,7 @@ class SignupPage extends Component {
             password: '',
             email: '',
             department: '',
-            empCode: ''
+            username: ''
         })
 
     }
